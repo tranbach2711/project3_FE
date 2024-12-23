52f83d6068380959158e2859cbb741d82a39cac8
VIBESUNDMIN\phong <phongnguyen195p2001@gmail.com>
2024-12-23 13:02:21 +0700

    add login re fe

--- src/Auth/SignIn.jsx
+++ src/Auth/SignIn.jsx
@@ -7,8 +7,8 @@ import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
   const [form] = Form.useForm();
-  const [username, setUsername] = useState(""); 
-  const [password, setPassword] = useState(""); 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
 
   const navigate = useNavigate();
@@ -18,10 +18,10 @@ const SignIn = () => {
     try {
 
       const response = await axios.post("http://localhost:5169/api/User/Login", {
-        User: username,
-        Password: password,
+        user: username,
+        password: password,
       });
-      console.log(response.data)
+
       if (response.data) {
         sessionStorage.setItem("userSession", JSON.stringify(response.data));
 
@@ -42,63 +42,79 @@ const SignIn = () => {
   };
 
   return (
-    <div style={{ maxWidth: "400px", margin: "auto", marginTop: "50px" }}>
-
+    // <div className="grid grid-cols-2 gap-4">
+    //   <div>
+    //     <img
+    //       src="https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&w=600"
+    //       alt=""
+    //       className="w-full h-full object-cover"
+    //     />
+    //   </div>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        Sign in to your account
+      </h2>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <Form
+              className="space-y-6"
+              form={form}
+              name="login"
+              layout="vertical"
+              initialValues={{ remember: true }}
+              autoComplete="off"
+              onFinish={onFinish}
+            >
+
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: "Nhập tên người dùng!" }]}
+              >
+                <Input
+                  placeholder="Nhập tên người dùng"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Item>
 
 
-      <Form
-        form={form}
-        name="login"
-        layout="vertical"
-        initialValues={{ remember: true }}
-        autoComplete="off"
-        onFinish={onFinish}
-      >
-
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Nhập tên người dùng!" }]}
-        >
-          <Input
-            placeholder="Nhập tên người dùng"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Item>
+              <Form.Item
+                label="Mật khẩu"
+                name="password"
+                rules={[{ required: true, message: "Nhập mật khẩu!" }]}
+              >
+                <Input.Password
+                  placeholder="Nhập mật khẩu"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)} // Two-way binding
+                />
+              </Form.Item>
 
 
-        <Form.Item
-          label="Mật khẩu"
-          name="password"
-          rules={[{ required: true, message: "Nhập mật khẩu!" }]}
-        >
-          <Input.Password
-            placeholder="Nhập mật khẩu"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} // Two-way binding
-          />
-        </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" block>
+                  Đăng nhập
+                </Button>
+              </Form.Item>
 
+              <Form.Item>
+                <div className="text-center mt-4">
+                  <span className="text-sm text-gray-500">Bạn chưa có tài khoản? </span>
+                  <Link to="/auth/signup" className="text-sm text-blue-500 hover:text-blue-700">
+                    Đăng ký ngay
+                  </Link>
+                </div>
+              </Form.Item>
+            </Form>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block>
-            Đăng nhập
-          </Button>
-        </Form.Item>
-
-        <Form.Item>
-          <div className="text-center mt-4">
-            <span className="text-sm text-gray-500">Bạn chưa có tài khoản? </span>
-            <Link to="/auth/signup" className="text-sm text-blue-500 hover:text-blue-700">
-              Đăng ký ngay
-            </Link>
           </div>
-        </Form.Item>
-      </Form>
-
+        </div>
+      </div>
     </div>
+    // </div>
   );
 };
 
--- src/Auth/SignUp.jsx
+++ src/Auth/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Button, Form, Input, Alert, notification } from "antd";
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
@@ -25,7 +26,7 @@ const SignUp = () => {
                 status: "00",
 
             });
-            if(response.data.code=="00"){
+            if (response.data.code == "00") {
                 notification.success({
                     message: 'Đăng ký thành công',
                     description: 'Bạn đã đăng ký thành công. Vui lòng đăng nhập.',
@@ -33,7 +34,7 @@ const SignUp = () => {
                 setTimeout(() => {
                     navigate("/auth");
                 }, 2000)
-            }else {
+            } else {
                 setErrorMessage(response.data.error);;
             }
         } catch (error) {
@@ -42,71 +43,89 @@ const SignUp = () => {
     };
 
     return (
-        <div>
-            {errorMessage && <Alert message={errorMessage} type="error" showIcon />}
-            <Form
-                name="register"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: 600 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Fullname"
-                    name="fullname"
-                    rules={[{ required: true, message: "Nhập họ và tên người dùng" }]}
-                > 
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: "Nhập tên người dùng" }]}
-                >
-                    <Input />
-                </Form.Item>
+        <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Sign up to your account
+            </h2>
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                    <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                        {errorMessage && <Alert message={errorMessage} type="error" showIcon />}
+                        <Form
+                            name="register"
+                            labelCol={{ span: 8 }}
+                            layout="vertical"
+                            // wrapperCol={{ span: 16 }}
+                            // style={{ maxWidth: 600 }}
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item
+                                label="Fullname"
+                                name="fullname"
+                                rules={[{ required: true, message: "Nhập họ và tên người dùng" }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[{ required: true, message: "Nhập tên người dùng" }]}
+                            >
+                                <Input />
+                            </Form.Item>
 
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                        { 
-                            required: true, 
-                            message: "Nhập email!" 
-                        },
-                        {
-                            type: "email", 
-                            message: "Địa chỉ email không hợp lệ!" 
-                        }
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+                            <Form.Item
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Nhập email!"
+                                    },
+                                    {
+                                        type: "email",
+                                        message: "Địa chỉ email không hợp lệ!"
+                                    }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
 
-                <Form.Item
-                    label="Mật khẩu"
-                    name="password"
-                    rules={[{ required: true, message: "Nhập mật khẩu" }]}
-                >
-                    <Input.Password />
-                </Form.Item>
+                            <Form.Item
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: "Nhập mật khẩu" }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
 
-                <Form.Item
-                    label="Xác nhận mật khẩu"
-                    name="passwordAgain"
-                    rules={[{ required: true, message: "Nhập lại mật khẩu" }]}
-                >
-                    <Input.Password />
-                </Form.Item>
+                            <Form.Item
+                                label="Xác nhận mật khẩu"
+                                name="passwordAgain"
+                                rules={[{ required: true, message: "Nhập lại mật khẩu" }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
 
-                <Form.Item label={null}>
-                    <Button htmlType="submit">
-                        Đăng ký
-                    </Button>
-                </Form.Item>
-            </Form>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" block>
+                                    Đăng ký
+                                </Button>
+                            </Form.Item>
+                            <Form.Item>
+                                <div className="text-center mt-4">
+                                    <span className="text-sm text-gray-500">Bạn đã có tài khoản? </span>
+                                    <Link to="/auth" className="text-sm text-blue-500 hover:text-blue-700">
+                                        Đăng nhập ngay
+                                    </Link>
+                                </div>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
